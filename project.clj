3d3c82c5c2251338381df45e9fae0df9cf6779ad
Markus Kauppila <markus.kauppila@gmail.com>
2016-11-29 18:58:59 +0200

    Add Http-kit and Cheshire

--- project.clj
+++ project.clj
@@ -4,7 +4,9 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [compojure "1.5.1"]
-                 [ring/ring-defaults "0.2.1"]]
+                 [ring/ring-defaults "0.2.1"]
+                 [http-kit "2.1.16"]
+                 [cheshire "5.6.3"]]
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler weather.handler/app}
   :profiles
