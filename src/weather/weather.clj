ee3846540b0b97aedff6f5f09aa1cd9849f4b84d
Markus Kauppila <markus.kauppila@gmail.com>
2016-12-06 18:35:17 +0200

    Better request to DarkSky API
    - Define the unit system
    - Define the used language
    - Exclude not used sections

--- src/weather/weather.clj
+++ src/weather/weather.clj
@@ -10,10 +10,21 @@
 (defn coordinate [latitude longitude]
   (str latitude "," longitude))
 
+(defn exclude-sections [blocks]
+  (str "exclude=" (clojure.string/join "," blocks)))
+
 (defn request-current-weather [latitude longitude]
-  (http/get (str (base-url-with-token) "/" (coordinate latitude longitude))))
+  (http/get (str
+    (base-url-with-token)
+    "/"
+    (coordinate latitude longitude)
+    "?"
+    (exclude-sections ["minutely" "hourly" "daily" "alerts" "flags"])
+    "&"
+    "units=si"
+    "&"
+    "language=en")))
 
 (defn current-weather [latitude longitude]
   (let [req @(request-current-weather latitude longitude)]
-    (println req)
-    (:body req)))
+    {:body (:body req)}))
